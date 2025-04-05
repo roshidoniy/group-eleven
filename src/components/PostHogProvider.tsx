b1719918dde9b9746e15982c043f88f562c189ec
roshidoniy <samadbek.mahmudjonov@gmail.com>
2025-04-05 16:17:12 +0500

    fixed security Vulnerabilities

--- src/components/PostHogProvider.tsx
+++ src/components/PostHogProvider.tsx
@@ -15,8 +15,6 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
     })
   }, [])
 
-  console.log(process.env.NEXT_PUBLIC_POSTHOG_KEY)
-
   return (
     <PHProvider client={posthog}>
       <SuspendedPostHogPageView />
