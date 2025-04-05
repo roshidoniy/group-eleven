b1863b0aba62ac32ffd3d7cc2e11604eeca3245d
roshidoniy <samadbek.mahmudjonov@gmail.com>
2025-04-05 16:31:49 +0500

    fixed security Vulnerabilities

--- src/components/PostHogProvider.tsx
+++ src/components/PostHogProvider.tsx
@@ -9,12 +9,11 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: "/ingest",
-      ui_host: "https://eu.posthog.com",
+      ui_host: "https://eu.i.posthog.com",
       capture_pageview: false, // We capture pageviews manually
       capture_pageleave: true, // Enable pageleave capture
     })
-  }, [])
-
+  }, []) 
   return (
     <PHProvider client={posthog}>
       <SuspendedPostHogPageView />
