a2297527d5def328fdd41642f8714a4a863b6fe1
roshidoniy <samadbek.mahmudjonov@gmail.com>
2025-04-18 07:01:11 +0500

    PWA was added

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,33 @@
 import type { NextConfig } from "next";
+import nextPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = nextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig: NextConfig = withPWA({
+  reactStrictMode: true, // Recommended
   async rewrites() {
     return [
       {
         source: "/ingest/static/:path*",
-        destination: "https://eu-assets.i.posthog.com/static/:path*"
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
       },
       {
         source: "/ingest/:path*",
-        destination: "https://eu.i.posthog.com/:path*"
+        destination: "https://eu.i.posthog.com/:path*",
       },
       {
         source: "/ingest/decide",
-        destination: "https://eu.i.posthog.com/decide"
-      }
+        destination: "https://eu.i.posthog.com/decide",
+      },
     ];
   },
   // This is required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true
-};
+  skipTrailingSlashRedirect: true,
+});
 
 export default nextConfig;
