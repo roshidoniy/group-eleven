68fd042414b36ee0209e87fa3ea44cf3b8e32e89
roshidoniy <samadbek.mahmudjonov@gmail.com>
2025-04-25 03:35:57 +0500

    updates url

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -29,7 +29,7 @@ export const courses: Course[] = [
     link: "https://teams.microsoft.com/dl/launcher/launcher.html?url=/_%23/l/meetup-join/19:meeting_MjI4YzQwZjQtMWVjMS00Yzg1LWJjNGUtZTdhYzhkODBlODE0@thread.v2/0?context%3D%257b%2522Tid%2522%253a%2522e8a52afe-6ea8-47f7-b275-783f7087b5fa%2522%252c%2522Oid%2522%253a%25223de94aa0-3474-472a-b197-bfd262bebca7%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=30ac6017-d190-4f7b-b10b-0c596d29630e&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true",
     time: { hour: 14, min: 40, weekdayNumber: 1 },
     lab: {
-      link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_NDU2ZjM0YTItYjZkZC00MWU1LWI0NDktZDM2YmVlODc2ZTQ4%40thread.v2/0?context=%7b%22Tid%22%3a%22e8a52afe-6ea8-47f7-b275-783f7087b5fa%22%2c%22Oid%22%3a%22ad1464e6-f406-49ef-8ff1-b5ab1743acc9%22%7d",
+      link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZTdmYTQ4NGYtMjI4MC00ZTQ2LWI0NzUtM2JjNTA3YTUyZTFh%40thread.v2/0?context=%7b%22Tid%22%3a%22e8a52afe-6ea8-47f7-b275-783f7087b5fa%22%2c%22Oid%22%3a%22ad1464e6-f406-49ef-8ff1-b5ab1743acc9%22%7d",
       time: { hour: 16, min: 50, weekdayNumber: 4 },
     },
   },
